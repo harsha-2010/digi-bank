61a86458550e89174c8d40c57b05f47741a6fec5
harsha <harsha@gmail.com>
2023-12-10 03:19:37 +0530

    Git Upload

--- src/app/app.animations.ts
+++
@@ -1,9 +0,0 @@
-// app.animations.ts
-import { animate, style, transition, trigger } from '@angular/animations';
-
-export const rippleAnimation = trigger('rippleAnimation', [
-  transition(':enter', [
-    style({ backgroundColor: 'rgba(42, 42, 42, 0.7)' }),
-    animate('1s ease-in', style({ backgroundColor: 'rgba(42, 42, 42, 0)' })),
-  ]),
-]);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
 // home.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+declare const particlesJS: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   constructor(private router: Router) {}
+
+  ngOnInit(): void{ 
+    particlesJS.load('particles', 'assets/particles.json', () => {
+      console.log("particles.js config loaded");
+    });
+  }
 
   redirectToLogin(): void {
     this.router.navigate(['/login']);
